91a6259dbde7a74d735ef91e96ffdcd5a76fa8a2
Dipta Das <dipta670@gmail.com>
2019-10-26 04:42:11 -0500

    Removed clossed sessions

--- src/main/java/edu/baylor/ecs/csi5324/ws/RawWsHandler.java
+++ src/main/java/edu/baylor/ecs/csi5324/ws/RawWsHandler.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import edu.baylor.ecs.csi5324.model.ProduceMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -42,5 +43,16 @@ public class RawWsHandler extends TextWebSocketHandler {
     public void afterConnectionEstablished(WebSocketSession session) {
         log.info("Connected");
         sessions.add(session);
+        try {
+            broadcast(new ProduceMessage("connected"));
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        log.info("Closed");
+        sessions.remove(session);
     }
 }
